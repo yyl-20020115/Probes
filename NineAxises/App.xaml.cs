c1e31c597c8eb4aec5c49107071788980450c9c7
john2015 <yyl_20050115@hotmail.com>
2019-04-28 12:52:47 +0800

    quad fixed

--- NineAxises/App.xaml.cs
+++ NineAxises/App.xaml.cs
@@ -13,5 +13,9 @@ namespace Probes
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+
+        }
     }
 }
--- NineAxises/QuadPosMeasurementNetControl.xaml.cs
+++ NineAxises/QuadPosMeasurementNetControl.xaml.cs
@@ -11,13 +11,13 @@ namespace Probes
     /// <summary>
     /// QuadPosMeasurementNetControl.xaml 的交互逻辑
     /// </summary>
-    public partial class QuadPosMeasurementNetControl : MeasurementBaseNetControl
+    public partial class QuadPosMeasurementNetControl : MeasurementBaseSerialControl
     {
         public override int ReceivePartLength => 24;
         protected override int LinesGroupLength => 2;
         public override string[] Headers => new string[] { };
         public override char EndOfLineChar => '\0';
-            
+
         protected override Grid LinesGrid => this.Lines;
         protected override CheckBox PauseCheckBox => this.Pause;
         protected override ComboBox RemoteAddressComboBox => this._RemoteAddressComboBox;
@@ -42,9 +42,57 @@ namespace Probes
             this.CommandTimer.Interval = DefaultCommandInterval;
             this.CommandTimer.Tick += Timer_Tick;
         }
+        private void DrawLine(Canvas canvas, Point Sp, Point Ep)
+        {
+            if (canvas != null)
+            {
+                var linegemetry = new LineGeometry
+                {
+                    StartPoint = Sp,
+                    EndPoint = Ep
+                };
+                var path = new Path
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1,
+                    Data = linegemetry
+                };
+                canvas.Children.Add(path);
+            }
+        }
+
+        private void DrawCircle(Canvas canvas, Point center,double radius)
+        {
+            if (canvas != null)
+            {
+                var ellipseGeometry = new EllipseGeometry
+                {
+                    Center = center,
+                    RadiusX = radius,
+                    RadiusY = radius
+                };
+                var path = new Path
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1,
+                    Data = ellipseGeometry
+                };
+                canvas.Children.Add(path);
+            }
+        }
         protected override void CallInitializeComponent()
         {
             InitializeComponent();
+
+            this.DrawLine(this.PositionCanvas, new Point(100.0, 0.0), new Point(100.0, 200.0));
+            this.DrawLine(this.PositionCanvas, new Point(0.0, 100.0), new Point(200.0, 100.0));
+
+            var cp = new Point(100.0, 100.0);
+
+            for (int radius = 10; radius <= 100; radius += 10)
+            {
+                this.DrawCircle(this.PositionCanvas, cp, radius);
+            }
         }
         protected virtual void Timer_Tick(object sender, System.EventArgs e)
         {
@@ -52,16 +100,17 @@ namespace Probes
         }
         protected int Translate(byte[] data, uint start)
         {
-            return data!=null && data.Length>=start + 6  
-                ? (data[start + 0] << 20) | (data[start + 1] << 16) | (data[start + 2] << 12) | (data[start + 3] << 8) | (data[start+4]<<4)|data[start + 5]<<0 
+            return data != null && data.Length >= start + 6
+                ? (data[start + 0] << 20) | (data[start + 1] << 16) | (data[start + 2] << 12) | (data[start + 3] << 8) | (data[start + 4] << 4) | (data[start + 5] << 0)
                 : 0;
         }
+        
         protected override void OnReceivedInternal(string input)
         {
             if(input!=null && input.Length == 24)
             {
-                byte[] data = Encoding.ASCII.GetBytes(input);
-                bool valid = true;
+                var data = Encoding.ASCII.GetBytes(input);
+                var valid = true;
                 for(int i = 0; i < data.Length; i++)
                 {
                     if (data[i] >= 48 && data[i] <= 48 + 15)
@@ -76,23 +125,21 @@ namespace Probes
                 }
                 if (valid)
                 {
-                    double D00 = this.Translate(data, 0);
-                    double D01 = this.Translate(data, 6);
-                    double D10 = this.Translate(data, 12);
-                    double D11 = this.Translate(data, 18);
-                    double DXX = D00 + D01 + D10 + D11;
+                    var D00 = this.Translate(data, 0);
+                    var D01 = this.Translate(data, 6);
+                    var D10 = this.Translate(data, 12);
+                    var D11 = this.Translate(data, 18);
+                    var DXX = D00 + D01 + D10 + D11;
 
-                    double x = (D01 + D11 - D00 - D10) / DXX;
-                    double y = (D10 + D11 - D00 - D01) / DXX;
-
+                    var x = DXX < 1000 ? 0.0 : (D01 + D11 - D00 - D10) * 1.0 / DXX;
+                    var y = DXX < 1000 ? 0.0 : (D10 + D11 - D00 - D01) * 1.0 / DXX;
 
                     this.AddData(x, LineIndex: 0, Update: true);
                     this.AddData(y, LineIndex: 1, Update: true);
-
-                    double px = 100.0 + x * 100.0;
-                    double py = 100.0 + y * 100.0;
+                     
+                    var px = 100.0 + x * 100.0;
+                    var py = 100.0 + y * 100.0;
                     
-
                     Canvas.SetLeft(EL, px - EL.Width / 2.0);
                     Canvas.SetTop(EL, py - EL.Height / 2.0);
                 }
@@ -102,7 +149,6 @@ namespace Probes
         {
             base.SetRemoteCheckBox_Checked(sender, e);
             this.CommandTimer.Start();
-
         }
         protected override void SetRemoteCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
